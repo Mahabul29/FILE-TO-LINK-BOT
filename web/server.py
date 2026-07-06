3d7a0274585e0ff1651bc42678b6ca0319e81a9b
Mahabul201 <Mahabulbhai288550@gmail.com>
2026-07-06 16:30:59 +0530

    Update server.py

--- web/server.py
+++ web/server.py
@@ -1,8 +1,8 @@
-
 import logging
 from aiohttp import web
 from config import PORT
 from web.video_play import video_play, stream_handler, download_handler
+from web.home import home_page
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -17,6 +17,7 @@ async def web_server(bot_client):
     app["bot_client"] = bot_client
 
     app.router.add_get("/", home)
+    app.router.add_get("/files", home_page)
     app.router.add_get("/watch/{file_id}", video_play)
     app.router.add_get("/stream/{file_id}", stream_handler)
     app.router.add_get("/dl/{file_id}", download_handler)
