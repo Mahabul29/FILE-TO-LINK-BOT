ad70d59c371c8d322bc610f1f6db7987987ff11f
Mahabul201 <Mahabulbhai288550@gmail.com>
2026-07-06 16:41:33 +0530

    Update server.py

--- web/server.py
+++ web/server.py
@@ -8,16 +8,11 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
-async def home(request):
-    return web.Response(text="✅ Bot is Running!")
-
-
 async def web_server(bot_client):
     app = web.Application(client_max_size=30 * 1024 * 1024)
     app["bot_client"] = bot_client
 
-    app.router.add_get("/", home)
-    app.router.add_get("/files", home_page)
+    app.router.add_get("/", home_page)
     app.router.add_get("/watch/{file_id}", video_play)
     app.router.add_get("/stream/{file_id}", stream_handler)
     app.router.add_get("/dl/{file_id}", download_handler)
