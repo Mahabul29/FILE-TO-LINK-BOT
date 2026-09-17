42bd279ceb63184871133910744d8c6fee47229d
Mahabul201 <Mahabulbhai288550@gmail.com>
2026-09-17 19:27:18 +0530

    Add new route for streaming files with filename

--- web/server.py
+++ web/server.py
@@ -16,6 +16,7 @@ async def web_server(bot_client):
     app.router.add_get("/", home_page)
     app.router.add_get("/watch/{file_id}", video_play)
     app.router.add_get("/stream/{file_id}", stream_handler)
+    app.router.add_get("/stream/{file_id}/{filename}", stream_handler)
     app.router.add_get("/dl/{file_id}", download_handler)
     app.router.add_get("/open/{player}/{file_id}", open_in_player)
 
