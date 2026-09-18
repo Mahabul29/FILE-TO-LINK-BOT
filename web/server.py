83ce8c42f05bfb632f491fa7b60d36631a58f0d0
Mahabul201 <Mahabulbhai288550@gmail.com>
2026-09-18 12:18:19 +0530

    Update server.py

--- web/server.py
+++ web/server.py
@@ -1,7 +1,8 @@
 import logging
 from aiohttp import web
 from config import PORT
-from web.video_play import video_player, stream_handler, download_handler
+# Change video_player to video_play on the import line below:
+from web.video_play import video_play, stream_handler, download_handler
 from web.home import home_page
 from web.open_redirect import open_in_player
 
@@ -14,7 +15,7 @@ async def web_server(bot_client):
     app["bot_client"] = bot_client
 
     app.router.add_get("/", home_page)
-    app.router.add_get("/watch/{file_id}", video_player)
+    app.router.add_get("/watch/{file_id}", video_play)  # Updated here
     app.router.add_get("/stream/{file_id}", stream_handler)
     app.router.add_get("/stream/{file_id}/{filename:.*}", stream_handler)
     app.router.add_get("/dl/{file_id}", download_handler)
